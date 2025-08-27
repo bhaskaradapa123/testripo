c98f014c178da0b1df8b75e451c40945001ea783
Bhaskar <bhaskar.adapa@gmail.com>
2025-08-27 22:16:28 +0530

    Copy selenium-test folder into testripo which is written googletest.java program.

---
+++ selenium-test/src/main/java/com/selenium/example/GoogleTest.java
@@ -0,0 +1,27 @@
+package com.selenium.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTest
+{
+    public static void main(String[] args)
+    {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.google.com");
+
+       WebElement searchBox = driver.findElement(By.xpath("//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']"));
+        searchBox.sendKeys("Selenium WebDriver");
+        searchBox.click();
+       System.out.println("Hello Selenium, Practise makes Perfect !! ");
+
+   }
+
+
+}
